52d42658846c1dc4d0fb566ebe5345eac9cba167
Piotr Wieczorek <p@wie.cz>
2025-04-10 19:14:21 +0200

    Fixes and expansions
    - add flags for controlling the verbosity of output
    - relax checking for "zero" exponent to take floating point shenanigans into consideration
    - add some more comments

--- main.go
+++ main.go
@@ -1,3 +1,16 @@
+/*
+unit_systems finds the "optimal" set of physical units that can express all
+the remaining ones.
+
+For calculating the score for candidate set of units it:
+1. Checks if every unit can be expressed as a linear combination of powers
+   of units from the candidate set.
+   If it cannot the candidate set is ignored.
+2. For each unit counts how many units from the candidate set are required
+   to express it, and adds the count to the score for the candidate set
+3. Prints the candidates with the minimum score
+*/
+
 package main
 
 import (
@@ -5,6 +18,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"math"
 	"os"
 
 	"gonum.org/v1/gonum/mat"
@@ -23,7 +37,11 @@ type Units struct {
 	Units   []Unit   `json:"units"`
 }
 
-var inputFlag = flag.String("i", "", "description of units")
+var inputFlag = flag.String("i", "", "path to json file with description of units")
+var verboseFlag = flag.Bool("v", false, "print score for every candidate set")
+var veryVerboseFlag = flag.Bool("vv", false, "print partial score for every candidate set")
+
+const epsilon = 0.0001
 
 func main() {
 	flag.Parse()
@@ -32,6 +50,68 @@ func main() {
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
+	veryVerbose := *veryVerboseFlag
+	verbose := veryVerbose || *verboseFlag
+
+	units := ReadUnitsFromFile(*inputFlag)
+	siUnitCount := len(units.SiUnits)
+
+	var bestUnits [][]string
+	var bestScore int
+	gen := combin.NewCombinationGenerator(len(units.Units), siUnitCount)
+combinations:
+	for gen.Next() {
+		score := 0
+		baseUnits := gen.Combination(nil)
+		unitNames := make([]string, siUnitCount)
+		m := mat.NewDense(siUnitCount, siUnitCount, nil)
+		for i, ix := range baseUnits {
+			unitNames[i] = units.Units[ix].Name
+			for j, coeff := range units.Units[ix].Coefficients {
+				m.Set(j, i, coeff)
+			}
+		}
+		// For every unit, try to express it in terms of the candidate base units.
+		if veryVerbose {
+			fmt.Println("Scoring units", unitNames)
+		}
+		for _, unit := range units.Units {
+			coeffs := mat.NewDense(siUnitCount, 1, unit.Coefficients)
+			resultSlice := make([]float64, siUnitCount)
+			result := mat.NewDense(siUnitCount, 1, resultSlice)
+			// Solve the linear system: m * x = coeffs.
+			// If the system does not have a solution, the candidate base units are not independent.
+			if err := result.Solve(m, coeffs); err != nil {
+				if verbose {
+					fmt.Println("Units", unitNames, "are not independent")
+				}
+				continue combinations
+			}
+			partialScore := 0
+			for _, resultCoeff := range resultSlice {
+				if math.Abs(resultCoeff) > epsilon {
+					partialScore += 1
+				}
+			}
+			score += partialScore
+			if veryVerbose {
+				fmt.Print("  +", partialScore, ", ", unit.Name, " = ", unitNames, " ⋅ ", resultSlice, "\n")
+			}
+		}
+		if verbose {
+			fmt.Println("Score for units", unitNames, "is", score)
+		}
+		if bestUnits == nil || score < bestScore {
+			bestUnits = [][]string{unitNames}
+			bestScore = score
+		} else if score == bestScore {
+			bestUnits = append(bestUnits, unitNames)
+		}
+	}
+	fmt.Println("Best units:", bestUnits, "score:", bestScore)
+}
+
+func ReadUnitsFromFile(path string) Units {
 	f, err := os.Open(*inputFlag)
 	if err != nil {
 		log.Fatal(err)
@@ -53,45 +133,5 @@ func main() {
 		log.Fatalf("there should be at least as many units as SI units (%d), but there are %d", siUnitCount, len(units.Units))
 	}
 
-	var bestUnits [][]string
-	var bestScore int
-	gen := combin.NewCombinationGenerator(len(units.Units), siUnitCount)
-combinations:
-	for gen.Next() {
-		score := 0
-		baseUnits := gen.Combination(nil)
-		unitNames := make([]string, siUnitCount)
-		m := mat.NewDense(siUnitCount, siUnitCount, nil)
-		for i, ix := range baseUnits {
-			unitNames[i] = units.Units[ix].Name
-			for j, coeff := range units.Units[ix].Coefficients {
-				m.Set(j, i, coeff)
-			}
-		}
-		// For every unit, try to express it in terms of the candidate base units.
-		for _, unit := range units.Units {
-			coeffs := mat.NewDense(siUnitCount, 1, unit.Coefficients)
-			resultSlice := make([]float64, siUnitCount)
-			result := mat.NewDense(siUnitCount, 1, resultSlice)
-			// Solve the linear system: m * x = coeffs.
-			// If the system does not have a solution, the candidate base units are not independent.
-			if err := result.Solve(m, coeffs); err != nil {
-				log.Print("Units ", unitNames, " are not independent")
-				continue combinations
-			}
-			for _, resultCoeff := range resultSlice {
-				if resultCoeff != 0 {
-					score += 1
-				}
-			}
-		}
-		fmt.Println("Score for units ", unitNames, " is ", score)
-		if bestUnits == nil || score < bestScore {
-			bestUnits = [][]string{unitNames}
-			bestScore = score
-		} else if score == bestScore {
-			bestUnits = append(bestUnits, unitNames)
-		}
-	}
-	fmt.Println("Best units: ", bestUnits, " score: ", bestScore)
+	return units
 }
